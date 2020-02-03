36a257d04885be2cae9acd9360b6d678a41e0874
daan <2087884@talnet.nl>
2020-02-03 13:27:23 +0100

    naam af

---
+++ name.php
@@ -0,0 +1,10 @@
+<?php 
+
+    echo ("wat is uw voornaam");
+    $voornaam = readline($prompt);
+    echo ("wat is uw achternaam");
+    $achternaam = readline($prompt);
+    echo ("$voornaam $achternaam")
+
+
+?>
